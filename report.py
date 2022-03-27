3ffc3853635446834bf819212e9fcbb2f86aa92f
Random Walker <sandyhxu@qq.com>
2022-03-27 21:53:28 +0800

    Cross-campus

--- report.py
+++ report.py
@@ -1,132 +1,129 @@
-import datetime
-import re
 import argparse
-from bs4 import BeautifulSoup
+import datetime
 import json
+import re
+
 import pytz
+from bs4 import BeautifulSoup
+
 from ustclogin2 import Login
 
 
 class Report(object):
-    def __init__(self, username, password, data_path, cont_name, cont_rel, cont_num):
-        self.username = username
-        self.password = password
-        self.data_path = data_path
-        self.cont_name = cont_name
-        self.cont_rel = cont_rel
-        self.cont_num = cont_num
+	def __init__(self, username, password, data_path, cont_name, cont_rel, cont_num):
+		self.username = username
+		self.password = password
+		self.data_path = data_path
+		self.cont_name = cont_name
+		self.cont_rel = cont_rel
+		self.cont_num = cont_num
 
-    def report(self):
-        login = Login(self.username, self.password, 'https://weixine.ustc.edu.cn/2020',
-                      'https://weixine.ustc.edu.cn/2020/caslogin', 'https://weixine.ustc.edu.cn/2020/home')
-        if login.login():
-            data = login.result.text
-            data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-            soup = BeautifulSoup(data, 'html.parser')
-            token = soup.find("input", {"name": "_token"})['value']
+	def report(self):
+		login = Login(self.username, self.password, 'https://weixine.ustc.edu.cn/2020/caslogin')
+		if login.login():
+			data = login.result.text
+			data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
+			soup = BeautifulSoup(data, 'html.parser')
+			token = soup.find("input", {"name": "_token"})['value']
 
-            with open(self.data_path, "r+", encoding='utf-8') as f:
-                data = f.read()
-                data = json.loads(data)
-                data["_token"] = token
-                if self.cont_name:
-                    data["jinji_lxr"] = self.cont_name
-                if self.cont_rel:
-                    data["jinji_guanxi"] = self.cont_rel
-                if self.cont_num:
-                    data["jiji_mobile"] = self.cont_num
-            headers = {
-                'authority': 'weixine.ustc.edu.cn',
-                'origin': 'https://weixine.ustc.edu.cn',
-                'upgrade-insecure-requests': '1',
-                'content-type': 'application/x-www-form-urlencoded',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'referer': 'https://weixine.ustc.edu.cn/2020/',
-                'accept-language': 'zh-CN,zh;q=0.9',
-                'Connection': 'close',
-                'cookie': "PHPSESSID=" + login.cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + login.cookies.get(
-                    "XSRF-TOKEN") + ";laravel_session=" + login.cookies.get("laravel_session"),
-            }
+			with open(self.data_path, "r+", encoding='utf-8') as f:
+				data = f.read()
+				data = json.loads(data)
+				data["_token"] = token
+				if self.cont_name:
+					data["jinji_lxr"] = self.cont_name
+				if self.cont_rel:
+					data["jinji_guanxi"] = self.cont_rel
+				if self.cont_num:
+					data["jiji_mobile"] = self.cont_num
+			headers = {
+				'authority': 'weixine.ustc.edu.cn',
+				'origin': 'https://weixine.ustc.edu.cn',
+				'upgrade-insecure-requests': '1',
+				'content-type': 'application/x-www-form-urlencoded',
+				'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
+				'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+				'referer': 'https://weixine.ustc.edu.cn/2020/',
+				'accept-language': 'zh-CN,zh;q=0.9',
+				'Connection': 'close',
+				'cookie': "PHPSESSID=" + login.cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + login.cookies.get(
+					"XSRF-TOKEN") + ";laravel_session=" + login.cookies.get("laravel_session"),
+			}
 
-            url = "https://weixine.ustc.edu.cn/2020/daliy_report"
-            resp = login.session.post(url, data=data, headers=headers)
-            data = login.session.get("https://weixine.ustc.edu.cn/2020").text
-            soup = BeautifulSoup(data, 'html.parser')
-            pattern = re.compile(
-                "202[0-9]-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
-            token = soup.find(
-                "span", {"style": "position: relative; top: 5px; color: #666;"})
-            flag = False
-            if pattern.search(token.text) is not None:
-                date = pattern.search(token.text).group()
-                print("Latest report: " + date)
-                date = date + " +0800"
-                reporttime = datetime.datetime.strptime(
-                    date, "%Y-%m-%d %H:%M:%S %z")
-                timenow = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
-                delta = timenow - reporttime
-                print("{} second(s) before.".format(delta.seconds))
-                if delta.seconds < 120:
-                    flag = True
+			url = "https://weixine.ustc.edu.cn/2020/daliy_report"
+			login.session.post(url, data=data, headers=headers)
+			data = login.session.get("https://weixine.ustc.edu.cn/2020").text
+			soup = BeautifulSoup(data, 'html.parser')
+			pattern = re.compile(
+				"202[0-9]-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
+			respalert = soup.find(
+				"span", {"style": "position: relative; top: 5px; color: #666;"})
+			flag = False
+			if pattern.search(respalert.text) is not None:
+				date = pattern.search(respalert.text).group()
+				print("Latest report: " + date)
+				date = date + " +0800"
+				reporttime = datetime.datetime.strptime(
+					date, "%Y-%m-%d %H:%M:%S %z")
+				timenow = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+				delta = timenow - reporttime
+				print("{} second(s) before.".format(delta.seconds))
+				if delta.seconds < 120:
+					flag = True
+					print("Daily Report SUCCESSFUL!")
+			if not flag:
+				print("Daily Report FAILED!")
+			data = login.session.get('https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=3', headers=headers).text
+			data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
+			soup = BeautifulSoup(data, 'html.parser')
+			start_date = soup.find("input", {"id": "start_date"})['value']
+			end_date = soup.find("input", {"id": "end_date"})['value']
 
-            # data = login.session.get('https://weixine.ustc.edu.cn/2020/apply/daliy/i', headers=headers).text
-            # data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-            # soup = BeautifulSoup(data, 'html.parser')
-            # token = soup.find("input", {"name": "_token"})['value']
-
-            data = login.session.get('https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=3', headers=headers).text
-            data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-            soup = BeautifulSoup(data, 'html.parser')
-            start_date = soup.find("input", {"id": "start_date"})['value']
-            end_date = soup.find("input", {"id": "end_date"})['value']
-            data = {
-                '_token': token,
-                'start_date': start_date,
-                'end_date': end_date,
-                'return_college[]': '东校区',
-                'return_college[]': '西校区',
-                'return_college[]': '南校区',
-                'return_college[]': '北校区',
-                'return_college[]': '中校区',
-                't': '3'}
-            post = login.session.post('https://weixine.ustc.edu.cn/2020/apply/daliy/post', data=data)
-            if post.url != 'https://weixine.ustc.edu.cn/2020/apply_total?t=d':
-                flag = False
-
-            if flag:
-                print("Report SUCCESSFUL!")
-            else:
-                print("Report FAILED!")
-            return flag
-        else:
-            return False
+			data = {
+				'_token': token,
+				'start_date': start_date,
+				'end_date': end_date,
+				'return_college[]': '东校区',
+				'return_college[]': '西校区',
+				'return_college[]': '南校区',
+				'return_college[]': '北校区',
+				'return_college[]': '中校区',
+				't': '3'}
+			post = login.session.post('https://weixine.ustc.edu.cn/2020/apply/daliy/post', data=data)
+			if post.url == 'https://weixine.ustc.edu.cn/2020/apply_total?t=d':
+				print("Cross-campus Report SUCCESSFUL!")
+			else:
+				flag = False
+				print("Cross-campus Report FAILED!")
+			return flag
+		else:
+			return False
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='URC nCov auto report script.')
-    parser.add_argument('username', help='your student number', type=str)
-    parser.add_argument('password', help='your CAS password', type=str)
-    parser.add_argument(
-        '--data_path', help='path to your own data used for post method', type=str, default='data.json')
-    parser.add_argument(
-        '--cont_name', help='your contact\'s name', type=str)
-    parser.add_argument(
-        '--cont_rel', help='your contact\'s relationship with you', type=str)
-    parser.add_argument(
-        '--cont_num', help='your contact\'s number', type=str)
-    args = parser.parse_args()
-    autoreporter = Report(username=args.username, password=args.password, data_path=args.data_path,
-                          cont_name=args.cont_name, cont_rel=args.cont_rel, cont_num=args.cont_num)
-    count = 1
-    while count != 11:
-        ret = autoreporter.report()
-        if ret:
-            break
-        print("*\n* Run " + count + " Failed, retry...\n*")
-        count = count + 1
-    if count != 11:
-        exit(0)
-    else:
-        exit(-1)
+	parser = argparse.ArgumentParser(
+		description='URC nCov auto report script.')
+	parser.add_argument('username', help='your student number', type=str)
+	parser.add_argument('password', help='your CAS password', type=str)
+	parser.add_argument(
+		'--data_path', help='path to your own data used for post method', type=str, default='data.json')
+	parser.add_argument(
+		'--cont_name', help='your contact\'s name', type=str)
+	parser.add_argument(
+		'--cont_rel', help='your contact\'s relationship with you', type=str)
+	parser.add_argument(
+		'--cont_num', help='your contact\'s number', type=str)
+	args = parser.parse_args()
+	autoreporter = Report(username=args.username, password=args.password, data_path=args.data_path,
+	                      cont_name=args.cont_name, cont_rel=args.cont_rel, cont_num=args.cont_num)
+	count = 1
+	while count != 11:
+		ret = autoreporter.report()
+		if ret:
+			break
+		print("*\n* Run {0} Failed, retry...\n*".format(count))
+		count = count + 1
+	if count != 11:
+		exit(0)
+	else:
+		exit(-1)
--- ustclogin2.py
+++ ustclogin2.py
@@ -1,50 +1,48 @@
-from bs4 import BeautifulSoup
+from urllib.parse import unquote
+
 import requests
+from bs4 import BeautifulSoup
+
+headers = {
+	'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
 
 
 class Login:
-    def __init__(self, username, password, origin, service, exam):
-        self.username = username
-        self.password = password
-        self.origin = origin
-        self.service = service
-        self.exam = exam
+	def __init__(self, stuid, password, service):
+		self.stuid = stuid
+		self.password = password
+		self.service = service
 
-    def passport(self):
-        data = self.session.get(
-            'https://passport.ustc.edu.cn/login?service='+self.service)
-        data = data.text
-        data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-        soup = BeautifulSoup(data, 'html.parser')
-        CAS_LT = soup.find("input", {"name": "CAS_LT"})['value']
-        self.session.get(
-            'https://passport.ustc.edu.cn/validatecode.jsp?type=login', stream=True)
-        data = {
-            'model': 'uplogin.jsp',
-            'service': self.service,
-            'warn': '',
-            'showCode': '0',
-            'username': self.username,
-            'password': str(self.password),
-            'button': '',
-            'CAS_LT': CAS_LT,
-            'LT': ''
-        }
-        self.session.post('https://passport.ustc.edu.cn/login', data=data)
+	def passport(self):
+		data = self.session.get('https://passport.ustc.edu.cn/login?service=' + self.service, headers=headers)
+		data = data.text
+		data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
+		soup = BeautifulSoup(data, 'html.parser')
+		CAS_LT = soup.find("input", {"name": "CAS_LT"})['value']
+		data = {
+			'model': 'uplogin.jsp',
+			'service': unquote(self.service),
+			'warn': '',
+			'showCode': '',
+			'username': self.stuid,
+			'password': str(self.password),
+			'button': '',
+			'CAS_LT': CAS_LT,
+			'LT': ''
+		}
+		self.result = self.session.post('https://passport.ustc.edu.cn/login', data=data, headers=headers)
 
-    def login(self):
-        self.session = requests.Session()
-        loginsuccess = False
-        retrycount = 5
-        while (not loginsuccess) and retrycount:
-            self.passport()
-            self.cookies = self.session.cookies
-            self.result = self.session.get(self.origin)
-            retrycount = retrycount - 1
-            if self.result.url != self.exam:
-                print("Login Failed! Retry...")
-            else:
-                print("Login Successful!")
-                loginsuccess = True
-        return loginsuccess
-
+	def login(self):
+		self.session = requests.Session()
+		loginsuccess = False
+		retrycount = 5
+		while (not loginsuccess) and retrycount:
+			self.passport()
+			self.cookies = self.session.cookies
+			retrycount = retrycount - 1
+			if self.result.url == 'https://passport.ustc.edu.cn/login':
+				print("Login Failed! Retry...")
+			else:
+				print("Login Successful!")
+				loginsuccess = True
+		return loginsuccess
