58bef6ef53fdf871732739a0f7f49dc6c988bfed
Random Walker <sandyhxu@qq.com>
2022-04-10 11:18:37 +0800

    Minor fixes

--- README.md
+++ README.md
@@ -1,16 +1,41 @@
-# 中国滑稽大学(University of Ridiculous of China)健康打卡平台自动打卡脚本
+## 中国滑稽大学(University of Ridiculous of China)健康打卡平台自动打卡脚本
 
 ## Current README
 
 Fork & grand rework of https://github.com/Kobe972/USTC-ncov-AutoReport.
 
-**Daily Report & Cross-campus Report supported; should work as of April 8, 2022.**
+**Daily Report & Cross-campus Report supported; should work as of April 10, 2022.**
 
-usage: report.py [-h] [--data_path DATAPATH] [--max_tries MAXTRIES] username password
+### Usage
+
+```
+report.py [-h] [--data_path DATAPATH] [--max_tries MAXTRIES] username password
+```
+
+Expected Output:
+```
+Login Successful!
+Got Daily Report Success Alert!
+Last report: HH小时MM分钟SS秒 before.
+Daily Report Successful!
+Cross-campus Report Successful! Until: yyyy-mm-dd HH:MM:SS
+```
+
+### GitHub Actions
+
+- Fork, or make a new repository with downloaded source code;
+
+- Settings > Secrets > Actions > New Secret > `USERNAME` & `PASSWORD` as student number and password;
+
+- Actions > `I understand my workflows, go ahead and enable them` > Auto-report action > `Run workflow` for test.
+
+See https://github.com/Kobe972/USTC-ncov-AutoReport for further instructions.
 
 ### Updates
-- **Fixed Cross-campus Report in light of April 5, 2022 change.**
+- Minor adjustments.
+- **Fixed Cross-campus Report in light of change of April 5, 2022.**
 - Fixed Daily Report & Cross-campus Report in light of March 2022 changes by URC.
 - Added try-catch logic, error logging and max_tries argument (default 10) as robustness measures.
 - Personal information (dorm, contact) is now primarily loaded from the previous report.
-- Now scheduled twice a day at 23:58 and 11:58 UTC+8; also has a `workflow_dispatch` event trigger (can be run manually, see [Actions page](actions/workflows/report.yml) after forking).
+- Now scheduled twice a day at **23:50 (~0:05)** and **22:00 (~22:10)** UTC+8.
+- Has a `workflow_dispatch` event trigger (can be run manually, see Actions after forking).
--- report.py
+++ report.py
@@ -8,137 +8,134 @@ from bs4 import BeautifulSoup
 from ustclogin2 import Login
 
 
-class Report(object):
-	def __init__(self, username, password, data_path):
-		self.username = username
-		self.password = password
-		self.data_path = data_path
-
-	def report(self, max_tries=10):
-		# Login
-		token = ''
-		for tries in range(max_tries):
-			try:
-				login = Login(self.username, self.password, 'https://weixine.ustc.edu.cn/2020/caslogin')
-				if login.login():
-					data = login.result.text
-					data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-					soup = BeautifulSoup(data, 'html.parser')
-					token = soup.find('input', {'name': '_token'})['value']
-					if token != '':
-						break
-			except:
-				print('* Error: Login: Get Token Failed in run {0}, detail as follows:'.format(tries))
-				print(traceback.format_exc())
-				continue
-		if not token:
-			print('*** FATAL ERROR\n*** Login Failed!\n*** Script Failed!\n')
-			return -1
-		# Constant Values
-		headers = {
-			'authority': 'weixine.ustc.edu.cn',
-			'origin': 'https://weixine.ustc.edu.cn',
-			'upgrade-insecure-requests': '1',
-			'content-type': 'application/x-www-form-urlencoded',
-			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
-			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-			'referer': 'https://weixine.ustc.edu.cn/2020/',
-			'accept-language': 'zh-CN,zh;q=0.9',
-			'Connection': 'close',
-			'cookie': 'PHPSESSID=' + login.cookies.get('PHPSESSID') + ';XSRF-TOKEN=' + login.cookies.get(
-				'XSRF-TOKEN') + ';laravel_session=' + login.cookies.get('laravel_session')
-		}
-		home_url = 'https://weixine.ustc.edu.cn/2020'
-		daily_url = 'https://weixine.ustc.edu.cn/2020/daliy_report'
-		cross_url = 'https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=3'
-		cross_return_url = 'https://weixine.ustc.edu.cn/2020/apply_total?t=d'
-		cross_post_url = 'https://weixine.ustc.edu.cn/2020/apply/daliy/post'
-		success_alert = '上报成功'
-		time_diff_pattern = re.compile('((0|[1-9]\d*)(年|月|周|星期|日|天|小时|时|分钟|分|秒))+')
-		# Read JSON
-		data = None
-		with open(self.data_path, 'r+', encoding='utf-8') as f:
-			data = json.loads(f.read())
-			data['_token'] = token
-		if not data:
-			print('*** FATAL ERROR\n*** Reading JSON Failed!\n*** Script Failed!\n')
-			return False
-		# Getting Previous Daily Report Data
+def report(username, password, data_path='./data.json', max_tries=10):
+	# Login
+	token = ''
+	login = None
+	for tries in range(max_tries):
 		try:
-			get = login.session.get(home_url, headers=headers)
-			soup = BeautifulSoup(get.text, 'html.parser')
-			jinji_lxr = soup.find('input', {'name': 'jinji_lxr'})['value']
-			jinji_guanxi = soup.find('input', {'name': 'jinji_guanxi'})['value']
-			jiji_mobile = soup.find('input', {'name': 'jiji_mobile'})['value']
-			dorm_building = soup.find('input', {'name': 'dorm_building'})['value']
-			dorm = soup.find('input', {'name': 'dorm'})['value']
-			data['jinji_lxr'] = jinji_lxr
-			data['jinji_guanxi'] = jinji_guanxi
-			data['jiji_mobile'] = jiji_mobile
-			data['dorm_building'] = dorm_building
-			data['dorm'] = dorm
+			login = Login(username, password, 'https://weixine.ustc.edu.cn/2020/caslogin')
+			if login.login():
+				data = login.result.text
+				data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
+				soup = BeautifulSoup(data, 'html.parser')
+				token = soup.find('input', {'name': '_token'})['value']
+				if token != '':
+					break
 		except:
-			print('* Warning: Daily Report: Getting Previous Report Data Failed, using default JSON:')
+			print('* Error: Login: Get Token Failed in run {0}, detail as follows:'.format(tries))
 			print(traceback.format_exc())
-
-		# Daily Report
-		ret = 2
-		for tries in range(max_tries):
-			try:
-				post = login.session.post(daily_url, data=data, headers=headers)
-				soup = BeautifulSoup(post.text, 'html.parser')
-				alert = soup.find('p', {'class': 'alert alert-success'})
-				if alert.text.find(success_alert) != -1:
-					print('Got Daily Report Success Alert!')
-					ret = 0
-				else:
-					print("* Warning: Daily Report: Failed to get success alert...")
-				if time_diff_pattern.search(alert.text):
-					time_diff = time_diff_pattern.search(alert.text).group()
-					print('Last report: {0} before.'.format(time_diff))
-					ret = 0
-				else:
-					print("* Warning: Daily Report: Failed to get last report time...")
-				if not ret:
-					print('Daily Report Successful!')
-					break
-				else:
-					print(
-						'* Error: Daily Report: Run {0} Finished but Failed to get response from alert, retrying...'.format(
-							tries))
-			except:
-				print('* Error: Daily Report: Run {0} Failed, detail as follows:'.format(tries))
-				print(traceback.format_exc())
-				continue
-		if ret:
-			print('* FATAL ERROR\n* Daily Report Failed!\n* Trying Cross-campus Report anyway...\n')
-
-		# Cross-campus Report
-		ret = ret | 4
-		for tries in range(max_tries):
-			try:
-				get = login.session.get(cross_url, headers=headers)
-				# get.text = get.text.encode('ascii', 'ignore').decode('utf-8', 'ignore')
-				soup = BeautifulSoup(get.text, 'html.parser')
-				start_date = soup.find('input', {'id': 'start_date'})['value']
-				end_date = soup.find('input', {'id': 'end_date'})['value']
-				data['start_date'] = start_date
-				data['end_date'] = end_date
-				post = login.session.post(cross_post_url, data=data, headers=headers)
-				if post.url == cross_return_url:
-					print('Cross-campus Report Successful! Until: {0}'.format(end_date))
-					ret = ret & -5
-					break
-				print('* Error: Cross-campus Report: Run {0} Finished but Failed to leave a record, retrying...'.format(
-					tries))
-			except:
-				print('* Error: Cross-campus Report: Run {0} Failed, detail as follows:'.format(tries))
-				print(traceback.format_exc())
-				continue
-		if ret & 4:
-			print('* FATAL ERROR\n* Cross-campus Report Failed!\n')
-
-		return -ret
+			continue
+	if not token or not login:
+		print('*** FATAL ERROR\n*** Login Failed!\n*** Script Failed!\n')
+		return 1
+	# Read JSON
+	with open(data_path, 'r+', encoding='utf-8') as f:
+		data = json.loads(f.read())
+		data['_token'] = token
+	if not data:
+		print('*** FATAL ERROR\n*** Reading JSON Failed!\n*** Script Failed!\n')
+		return 2
+	# Constant Values
+	headers = {
+		'authority': 'weixine.ustc.edu.cn',
+		'origin': 'https://weixine.ustc.edu.cn',
+		'upgrade-insecure-requests': '1',
+		'content-type': 'application/x-www-form-urlencoded',
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
+		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+		'referer': 'https://weixine.ustc.edu.cn/2020/',
+		'accept-language': 'zh-CN,zh;q=0.9',
+		'Connection': 'close',
+		'cookie': 'PHPSESSID=' + login.cookies.get('PHPSESSID') + ';XSRF-TOKEN=' + login.cookies.get(
+			'XSRF-TOKEN') + ';laravel_session=' + login.cookies.get('laravel_session')
+	}
+	cross_data = {"return_college[]": [
+		"中校区",
+		"西校区",
+		"南校区",
+		"北校区",
+		"东校区"
+	], "reason": "实验、自习", "t": "3", '_token': token}
+	home_url = 'https://weixine.ustc.edu.cn/2020'
+	daily_url = 'https://weixine.ustc.edu.cn/2020/daliy_report'
+	cross_url = 'https://weixine.ustc.edu.cn/2020/apply/daliy/i?t=3'
+	cross_return_url = 'https://weixine.ustc.edu.cn/2020/apply_total?t=d'
+	cross_post_url = 'https://weixine.ustc.edu.cn/2020/apply/daliy/post'
+	success_alert = '上报成功'
+	time_diff_pattern = re.compile('((0|[1-9]\\d*)(年|月|周|星期|日|天|小时|时|分钟|分|秒))+')
+	# Getting Previous Daily Report Data
+	try:
+		get = login.session.get(home_url, headers=headers)
+		soup = BeautifulSoup(get.text, 'html.parser')
+		jinji_lxr = soup.find('input', {'name': 'jinji_lxr'})['value']
+		jinji_guanxi = soup.find('input', {'name': 'jinji_guanxi'})['value']
+		jiji_mobile = soup.find('input', {'name': 'jiji_mobile'})['value']
+		dorm_building = soup.find('input', {'name': 'dorm_building'})['value']
+		dorm = soup.find('input', {'name': 'dorm'})['value']
+		data['jinji_lxr'] = jinji_lxr
+		data['jinji_guanxi'] = jinji_guanxi
+		data['jiji_mobile'] = jiji_mobile
+		data['dorm_building'] = dorm_building
+		data['dorm'] = dorm
+	except:
+		print('* Warning: Daily Report: Getting Previous Report Data Failed, using default JSON:')
+		print(traceback.format_exc())
+	# Daily Report
+	ret = 4
+	for tries in range(max_tries):
+		try:
+			post = login.session.post(daily_url, data=data, headers=headers)
+			soup = BeautifulSoup(post.text, 'html.parser')
+			alert = soup.find('p', {'class': 'alert alert-success'})
+			if alert.text.find(success_alert) != -1:
+				print('Got Daily Report Success Alert!')
+				ret = 0
+			else:
+				print("* Warning: Daily Report: Failed to get success alert...")
+			if time_diff_pattern.search(alert.text):
+				time_diff = time_diff_pattern.search(alert.text).group()
+				print('Last report: {0} before.'.format(time_diff))
+				ret = 0
+			else:
+				print("* Warning: Daily Report: Failed to get last report time...")
+			if not ret:
+				print('Daily Report Successful!')
+				break
+			else:
+				print(
+					'* Error: Daily Report: Run {0} Finished but Failed to get response from alert, retrying...'.format(
+						tries))
+		except:
+			print('* Error: Daily Report: Run {0} Failed, detail as follows:'.format(tries))
+			print(traceback.format_exc())
+			continue
+	if ret:
+		print('* FATAL ERROR\n* Daily Report Failed!\n* Trying Cross-campus Report anyway...\n')
+	# Cross-campus Report
+	ret = ret | 8
+	for tries in range(max_tries):
+		try:
+			get = login.session.get(cross_url, headers=headers)
+			soup = BeautifulSoup(get.text, 'html.parser')
+			start_date = soup.find('input', {'id': 'start_date'})['value']
+			end_date = soup.find('input', {'id': 'end_date'})['value']
+			cross_data['start_date'] = start_date
+			cross_data['end_date'] = end_date
+			post = login.session.post(cross_post_url, data=cross_data, headers=headers)
+			if post.url == cross_return_url:
+				print('Cross-campus Report Successful! Until: {0}'.format(end_date))
+				ret = ret & -9
+				break
+			print('* Error: Cross-campus Report: Run {0} Finished but Failed to leave a record, retrying...'.format(
+				tries))
+		except:
+			print('* Error: Cross-campus Report: Run {0} Failed, detail as follows:'.format(tries))
+			print(traceback.format_exc())
+			continue
+	if ret & 8:
+		print('* FATAL ERROR\n* Cross-campus Report Failed!\n')
+	return ret
 
 
 if __name__ == '__main__':
@@ -147,9 +144,9 @@ if __name__ == '__main__':
 	parser.add_argument('username', help='your student number', type=str)
 	parser.add_argument('password', help='your CAS password', type=str)
 	parser.add_argument(
-		'--data_path', help='path to your own data used for post method', type=str, default='data.json')
+		'--data_path', help='path to your own data.json', type=str, default='./data.json')
 	parser.add_argument(
-		'--max_tries', help='max tries as in Fixed Daily Report & Cross-campus Report', type=int, default=10)
+		'--max_tries', help='max tries as in reports', type=int, default=10)
 	args = parser.parse_args()
-	autoreporter = Report(username=args.username, password=args.password, data_path=args.data_path)
-	exit(autoreporter.report())
+	exit(report(username=args.username, password=args.password, data_path=args.data_path,
+	            max_tries=args.max_tries))
